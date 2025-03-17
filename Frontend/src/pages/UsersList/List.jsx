38125204c7fc0957336275882bbb3a833982734c
DJerowdMoreschi <djerowdalexsanderbr@gmail.com>
2025-03-17 09:39:29 -0300

    Users List pagination

--- Frontend/src/pages/UsersList/List.jsx
+++ Frontend/src/pages/UsersList/List.jsx
@@ -32,28 +32,6 @@ function List({ users, currentPage, setCurrentPage, itemsPerPage }) {
                 <th>{`${user.email}`}</th>
             </tr>
             ))}
-
-            {/* {totalPages > 1 && (
-                <div className="pagination">
-                    <button onClick={() => setCurrentPage(prev => Math.max(1, prev - 1))} disabled={currentPage === 1} >
-                        Anterior
-                    </button>
-                    
-                    {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNum) => (
-                        <button
-                            key={pageNum}
-                            onClick={() => setCurrentPage(pageNum)}
-                            className={currentPage === pageNum ? 'active' : ''}
-                        >
-                            {pageNum}
-                        </button>
-                    ))}
-                    
-                    <button onClick={() => setCurrentPage(prev => Math.min(totalPages, prev + 1))} disabled={currentPage === totalPages} >
-                        Próximo
-                    </button>
-                </div>
-            )} */}
         </ul>
     );
 }
